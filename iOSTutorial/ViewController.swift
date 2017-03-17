73e38e6416ddcef11cd6a702388c55aef1573208
Fazil <fazilt@gmail.com>
2017-03-17 12:16:51 -0700

    Initial Commit

---
+++ iOSTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSTutorial
+//
+//  Created by T, Fazil Muhammed on 3/17/17.
+//  Copyright © 2017 MobileDeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
